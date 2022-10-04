a6a28e16b245b871dd4097c27cd61fccf9ac79d1
dslgk <115045153+dslgk@users.noreply.github.com>
2022-10-05 00:08:22 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,40 @@
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+    const triggerBottom = (window.innerHeight / 4) * 2;
+    boxes.forEach((box) =>{
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) box.classList.add("show");
+        else box.classList.remove("show")
+    });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes()
+
+
+
+var mybotton = document.getElementById('goup');
+
+window.onscroll = function () {
+
+    // console.log(scrollY)
+    'user strict';
+
+    if (window.pageYOffset >= 1000){
+        mybotton.style.display = "block";        
+    }
+    else{
+        mybotton.style.display = "none";        
+    }
+
+};
+
+mybotton.onclick = function () {
+    'user strict';
+
+    window.scrollTo({
+        top: 0,       
+        behavior: "smooth",
+    });
+}
